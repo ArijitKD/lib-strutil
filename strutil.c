99ccbda7f7c3562e0e999691766a3669e2f71eb6
Arijit Kumar Das <arijitkdgit.official@gmail.com>
2025-02-19 01:39:28 +0530

    Added wide character support
    Bug fixes and optimizations

--- strutil.c
+++ strutil.c
@@ -2,74 +2,109 @@
 #include "strutil.h"
 
 // Perfected
-unsigned int equals (const char* string1, const char* string2)
+unsigned int equals (const LIBC_STRUTIL_CHARTYPE* string1, const LIBC_STRUTIL_CHARTYPE* string2)
 {
-    return (strcmp(string1, string2) == 0);
+    if (string1 == NULL || string2 == NULL)
+        return (string1 == string2);
+    return (STRCMP(string1, string2) == 0);
 }
 
 // Perfected
-char* copy (const char* string)
+LIBC_STRUTIL_CHARTYPE* copy (const LIBC_STRUTIL_CHARTYPE* string)
 {
-    char* strcopy = (char*)malloc((strlen(string)+1)*sizeof(char));
+    if (string == NULL)
+        return NULL;
+    LIBC_STRUTIL_CHARTYPE* strcopy = (LIBC_STRUTIL_CHARTYPE*)malloc((STRLEN(string)+1)*sizeof(LIBC_STRUTIL_CHARTYPE));
     if (strcopy == NULL)
         return NULL;
-    strcpy(strcopy, string);
+    STRCPY(strcopy, string);
     return strcopy;
 }
 
 // Perfected
-void reverse (char* string)
+void reverse (LIBC_STRUTIL_CHARTYPE* string)
 {
-    unsigned int len = strlen(string);
-    for (unsigned int i=0; i<len/2; ++i)
+    if (string != NULL)
     {
-        string[i] = string[i] + string[len-(i+1)];
-        string[len-(i+1)] = string[i] - string[len-(i+1)];
-        string[i] = string[i] - string[len-(i+1)];
+        size_t len = STRLEN(string);
+        for (size_t i=0; i<len/2; ++i)
+        {
+            string[i] = string[i] + string[len-(i+1)];
+            string[len-(i+1)] = string[i] - string[len-(i+1)];
+            string[i] = string[i] - string[len-(i+1)];
+        }
     }
 }
 
 // Perfected
-char* reversecopy (const char* string)
+LIBC_STRUTIL_CHARTYPE* reversecopy (const LIBC_STRUTIL_CHARTYPE* string)
 {
-    char* newstring = copy(string);
+    if (string == NULL)
+        return NULL;
+    LIBC_STRUTIL_CHARTYPE* newstring = copy(string);
+    if (newstring == NULL)
+        return NULL;
     reverse(newstring);
     return newstring;
 }
 
 // Perfected
-unsigned int startswith (const char* pattern, const char* string)
+int startswith (const LIBC_STRUTIL_CHARTYPE* pattern, const LIBC_STRUTIL_CHARTYPE* string)
 {
+    if (pattern == NULL || string == NULL)
+        return -1;
     if (*pattern == 0)
         return 1;
-    size_t patternlen = strlen(pattern), i = 0;
+    size_t patternlen = STRLEN(pattern), stringlen = STRLEN(string);
+    if (patternlen > stringlen)
+        return 0;
+    size_t i = 0;
     while (i < patternlen && pattern[i] == string[i])
         i++;
     return (i == patternlen);
 }
 
 // Perfected
-unsigned int endswith (const char* pattern, const char* string)
+int endswith (const LIBC_STRUTIL_CHARTYPE* pattern, const LIBC_STRUTIL_CHARTYPE* string)
 {
+    if (pattern == NULL || string == NULL)
+        return -1;
     if (*pattern == 0)
         return 1;
-    ssize_t j = strlen(pattern), i = strlen(string);
-    while (j >= 0 && pattern[--j] == string[--i]);
-    return (j == -1);
+    size_t j = STRLEN(pattern), i = STRLEN(string);
+    if (j > i)
+        return 0;
+    while (j > 0)
+        if (pattern[--j] != string[--i])
+            return 0;
+    return 1;
 }
 
 // TODO: Use Boyer-Moore algorithm with the current brute-force one and
 // implement a hybrid search algorithm
-ssize_t find (const char* pattern, const char* string)
+ssize_t find (const LIBC_STRUTIL_CHARTYPE* pattern, const LIBC_STRUTIL_CHARTYPE* string)
 {
+    if (pattern == NULL || string == NULL)
+        return -1;
     if (*pattern==0)
         return 0;
     if (*string==0 && *pattern!=0)
         return -1;
-    ssize_t patternlen = strlen(pattern);
-    ssize_t stringlen = strlen(string);
+    size_t patternlen = STRLEN(pattern);
+    size_t stringlen = STRLEN(string);
     if (patternlen > stringlen)
         return -1;
+
+    for (size_t i = 0; i <= stringlen - patternlen; ++i)
+    {
+        if (STRNCMP(&string[i], pattern, patternlen) == 0)
+            return i;
+    }
+    return -1;
+
+    // It doesn't feel good to ditch the code that you've spent your time and energy to write, does it?
+    // Commenting out my version, but using strncmp() version for better performance
+    /*
     ssize_t i, j, k, found_index = -1;
     for (i=0; i<stringlen; ++i)
     {
@@ -89,41 +124,65 @@ ssize_t find (const char* pattern, const char* string)
         }
     }
     return found_index;
+    */
 }
 
 // Perfected
-ssize_t indexof (const char* pattern, const char* string)
+ssize_t indexof (const LIBC_STRUTIL_CHARTYPE* pattern, const LIBC_STRUTIL_CHARTYPE* string)
 {
     return find(pattern, string);
 }
 
 // Perfected
-ssize_t rindexof (const char* pattern, const char* string)
+ssize_t rindexof (const LIBC_STRUTIL_CHARTYPE* pattern, const LIBC_STRUTIL_CHARTYPE* string)
 {
-    char* revpattern = reversecopy(pattern);
-    char* revstring = reversecopy(string);
+    if (pattern == NULL || string == NULL)
+        return -1;
+    if (*pattern==0)
+        return 0;
+    if (*string==0 && *pattern!=0)
+        return -1;
+    size_t patternlen = STRLEN(pattern);
+    size_t stringlen = STRLEN(string);
+    if (patternlen > stringlen)
+        return -1;
+    
+    for (ssize_t i = stringlen - patternlen; i >= 0; --i)
+    {
+        if (STRNCMP(&string[i], pattern, patternlen) == 0)
+            return i;
+    }
+
+    return -1;
+
+    /*
+    LIBC_STRUTIL_CHARTYPE* revpattern = reversecopy(pattern);
+    LIBC_STRUTIL_CHARTYPE* revstring = reversecopy(string);
     ssize_t found_index = indexof(revpattern, revstring);
     if (found_index != -1)
-        found_index = (strlen(revstring)-found_index)-strlen(revpattern);
+        found_index = (STRLEN(revstring)-found_index)-STRLEN(revpattern);
     free(revpattern);
     free(revstring);
     return found_index;
+    */
 }
 
 // Perfected
-char* substring (size_t start, size_t end, const char* string)
+LIBC_STRUTIL_CHARTYPE* substring (size_t start, size_t end, const LIBC_STRUTIL_CHARTYPE* string)
 {
+    if (string == NULL)
+        return NULL;
     if (equals(string, ""))
     {
         if (start == 0 && end == 0)
-            return (char*)calloc(1, sizeof(char));
+            return (LIBC_STRUTIL_CHARTYPE*)calloc(1, sizeof(LIBC_STRUTIL_CHARTYPE));
         else
             return NULL;
     }
-    size_t maxindex = strlen(string)-1;
+    size_t maxindex = STRLEN(string)-1;
     if (start > maxindex || end > maxindex || start > end)
         return NULL;
-    char* substr = (char*)malloc((end-start+1)*sizeof(char));
+    LIBC_STRUTIL_CHARTYPE* substr = (LIBC_STRUTIL_CHARTYPE*)malloc((end-start+1)*sizeof(LIBC_STRUTIL_CHARTYPE));
     if (substr == NULL)
         return NULL;
     for (size_t i=start, j=0; i<end; ++i, ++j)
--- strutil.h
+++ strutil.h
@@ -4,15 +4,36 @@
         #include <stddef.h>
         typedef ptrdiff_t ssize_t;  // win32 might not define ssize_t
     #endif
-    #include <string.h>
-    unsigned int equals (const char* string1, const char* string2);
-    char* copy (const char* string);
-    void reverse (char* string);
-    char* reversecopy (const char* string);
-    unsigned int startswith (const char* pattern, const char* string);
-    unsigned int endswith (const char* pattern, const char* string);
-    ssize_t find (const char* pattern, const char* string);
-    ssize_t indexof (const char* pattern, const char* string);
-    ssize_t rindexof (const char* pattern, const char* string);
-    char* substring (size_t start, size_t end, const char* string);
+
+    #ifdef LIBC_STRUTIL_WCHAR
+        #include <wchar.h>
+        #define STRLEN wcslen
+        #define STRCMP wcscmp
+        #define STRNCMP wcsncmp
+        #define STRCPY wcscpy
+        #define STRNCPY wcsncpy
+        #define STRSTR wcsstr
+        typedef unsigned short LIBC_STRUTIL_CHARTYPE;
+    #else
+        #include <string.h>
+        #define STRLEN strlen
+        #define STRCMP strcmp
+        #define STRNCMP strncmp
+        #define STRCPY strcpy
+        #define STRNCPY strncpy
+        #define STRSTR strstr
+        typedef unsigned char LIBC_STRUTIL_CHARTYPE;
+    #endif
+
+
+    unsigned int equals (const LIBC_STRUTIL_CHARTYPE* string1, const LIBC_STRUTIL_CHARTYPE* string2);
+    LIBC_STRUTIL_CHARTYPE* copy (const LIBC_STRUTIL_CHARTYPE* string);
+    void reverse (LIBC_STRUTIL_CHARTYPE* string);
+    LIBC_STRUTIL_CHARTYPE* reversecopy (const LIBC_STRUTIL_CHARTYPE* string);
+    int startswith (const LIBC_STRUTIL_CHARTYPE* pattern, const LIBC_STRUTIL_CHARTYPE* string);
+    int endswith (const LIBC_STRUTIL_CHARTYPE* pattern, const LIBC_STRUTIL_CHARTYPE* string);
+    ssize_t find (const LIBC_STRUTIL_CHARTYPE* pattern, const LIBC_STRUTIL_CHARTYPE* string);
+    ssize_t indexof (const LIBC_STRUTIL_CHARTYPE* pattern, const LIBC_STRUTIL_CHARTYPE* string);
+    ssize_t rindexof (const LIBC_STRUTIL_CHARTYPE* pattern, const LIBC_STRUTIL_CHARTYPE* string);
+    LIBC_STRUTIL_CHARTYPE* substring (size_t start, size_t end, const LIBC_STRUTIL_CHARTYPE* string);
 #endif
