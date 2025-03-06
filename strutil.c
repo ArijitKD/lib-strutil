f956e5886d8dbca6bf974d705fe30bd81efa8dfc
Arijit Kumar Das <arijitkdgit.official@gmail.com>
2025-03-07 02:57:17 +0530

    Refactor code

--- strutil.c
+++ strutil.c
@@ -1,77 +1,106 @@
+#include <stdbool.h>
 #include <stdlib.h>
 #include "strutil.h"
 
-// Perfected
-unsigned int equals (const STRUTIL_CHARTYPE* string1, const STRUTIL_CHARTYPE* string2)
+/*
+ * Checks if two strings are equal and returns 1 if true, 0 otherwise.
+ */
+bool equals(const STRUTIL_CHARTYPE *string1, const STRUTIL_CHARTYPE *string2)
 {
     if (string1 == NULL || string2 == NULL)
-        return (string1 == string2);
-    return (STRCMP(string1, string2) == 0);
+        return string1 == string2;
+    return STRCMP(string1, string2) == 0;
 }
 
-// Perfected
-STRUTIL_CHARTYPE* copy (const STRUTIL_CHARTYPE* string)
+/* 
+ * Returns a pointer to a shallow copy of the given string. This function dynamically allocates
+ * memory for the string. This memory may need to be freed by the caller to avoid memory
+ * leaks. If memory allocation fails or the function receives a NULL pointer, returns NULL.
+ */
+STRUTIL_CHARTYPE *copy(const STRUTIL_CHARTYPE *string)
 {
+    STRUTIL_CHARTYPE *strcopy;
+
     if (string == NULL)
         return NULL;
-    STRUTIL_CHARTYPE* strcopy = (STRUTIL_CHARTYPE*)malloc((STRLEN(string)+1)*sizeof(STRUTIL_CHARTYPE));
+    strcopy = (STRUTIL_CHARTYPE *)malloc(strbuflen(string) * sizeof(STRUTIL_CHARTYPE));
     if (strcopy == NULL)
         return NULL;
     STRCPY(strcopy, string);
     return strcopy;
 }
 
-// Perfected
-void reverse (STRUTIL_CHARTYPE* string)
+/*
+ * Reverses the given string. This function attempts to modify the given string. Make sure
+ * that the string being passed is a modifiable character array or a dynamically allocated
+ * character buffer. Use reversecopy() for read-only strings.
+ */
+void reverse(STRUTIL_CHARTYPE *string)
 {
-    if (string != NULL)
-    {
-        size_t len = STRLEN(string);
-        for (size_t i=0; i<len/2; ++i)
-        {
-            string[i] = string[i] + string[len-(i+1)];
-            string[len-(i+1)] = string[i] - string[len-(i+1)];
-            string[i] = string[i] - string[len-(i+1)];
+    size_t len, i;
+
+    if (string != NULL) {
+        len = STRLEN(string);
+        for (i = 0; i < len / 2; ++i) {
+            string[i] = string[i] + string[len - (i + 1)];
+            string[len - (i + 1)] = string[i] - string[len - (i + 1)];
+            string[i] = string[i] - string[len - (i + 1)];
         }
     }
 }
 
-// Perfected
-STRUTIL_CHARTYPE* reversecopy (const STRUTIL_CHARTYPE* string)
+/*
+ * Creates a new dynamically allocated copy of the given string, reverses it, and returns
+ * a pointer to the newly created buffer. Needs to be freed by the caller to prevent
+ * memory leaks. If memory allocation fails or the function receives a NULL pointer,
+ * returns NULL.
+ */
+STRUTIL_CHARTYPE *reversecopy(const STRUTIL_CHARTYPE *string)
 {
+    STRUTIL_CHARTYPE *newstring;
+
     if (string == NULL)
         return NULL;
-    STRUTIL_CHARTYPE* newstring = copy(string);
+    newstring = copy(string);
     if (newstring == NULL)
         return NULL;
     reverse(newstring);
     return newstring;
 }
 
-// Perfected
-int startswith (const STRUTIL_CHARTYPE* pattern, const STRUTIL_CHARTYPE* string)
+/*
+ * Check if the given string starts with the given pattern. Returns 1 if true, 0 otherwise.
+ */
+bool startswith(const STRUTIL_CHARTYPE *pattern, const STRUTIL_CHARTYPE *string)
 {
+    size_t patternlen, stringlen, i = 0;
+
     if (pattern == NULL || string == NULL)
         return -1;
     if (*pattern == 0)
         return 1;
-    size_t patternlen = STRLEN(pattern), stringlen = STRLEN(string);
+    patternlen = STRLEN(pattern);
+    stringlen = STRLEN(string);
     if (patternlen > stringlen)
         return 0;
-    size_t i = 0;
     while (i < patternlen && pattern[i] == string[i])
         i++;
-    return (i == patternlen);
+    return i == patternlen;
 }
 
-// Perfected
-int endswith (const STRUTIL_CHARTYPE* pattern, const STRUTIL_CHARTYPE* string)
+/*
+ * Check if the given string ends with the given pattern. Returns 1 if true, 0 otherwise.
+ */
+bool endswith(const STRUTIL_CHARTYPE *pattern, const STRUTIL_CHARTYPE *string)
 {
+    size_t j, i;
+
     if (pattern == NULL || string == NULL)
         return -1;
     if (*pattern == 0)
         return 1;
-    size_t j = STRLEN(pattern), i = STRLEN(string);
+    j = STRLEN(pattern);
+    i = STRLEN(string);
     if (j > i)
         return 0;
     while (j > 0)
@@ -80,115 +109,91 @@ int endswith (const STRUTIL_CHARTYPE* pattern, const STRUTIL_CHARTYPE* string)
     return 1;
 }
 
-// TODO: Use Boyer-Moore algorithm with the current brute-force one and
-// implement a hybrid search algorithm
-ssize_t find (const STRUTIL_CHARTYPE* pattern, const STRUTIL_CHARTYPE* string)
+/*
+ * Searches for the given pattern in the string and returns the index of the first matching character.
+ * If the pattern cannot be found, returns -1.
+ */
+ssize_t find(const STRUTIL_CHARTYPE *pattern, const STRUTIL_CHARTYPE *string)
 {
+    size_t patternlen, stringlen, i;
+
     if (pattern == NULL || string == NULL)
         return -1;
-    if (*pattern==0)
+    if (*pattern == 0)
         return 0;
-    if (*string==0 && *pattern!=0)
+    if (*string == 0 && *pattern != 0)
         return -1;
-    size_t patternlen = STRLEN(pattern);
-    size_t stringlen = STRLEN(string);
+    patternlen = STRLEN(pattern);
+    stringlen = STRLEN(string);
     if (patternlen > stringlen)
         return -1;
-
-    for (size_t i = 0; i <= stringlen - patternlen; ++i)
-    {
-        if (STRNCMP(&string[i], pattern, patternlen) == 0)
+    for (i = 0; i <= stringlen - patternlen; ++i) {
+        if (STRNCMP(string + i, pattern, patternlen) == 0)
             return i;
     }
     return -1;
-
-    // It doesn't feel good to ditch the code that you've spent your time and energy to write, does it?
-    // Commenting out my version, but using strncmp() version for better performance
-    /*
-    ssize_t i, j, k, found_index = -1;
-    for (i=0; i<stringlen; ++i)
-    {
-        if (string[i] == pattern[0])
-        {
-            k = i;
-            for (j=1; j<patternlen; ++j)
-            {
-                if (pattern[j] != string[++k])
-                    break;
-            }
-            if (j == patternlen)
-            {
-                found_index = i;
-                break;
-            }
-        }
-    }
-    return found_index;
-    */
 }
 
-// Perfected
-ssize_t indexof (const STRUTIL_CHARTYPE* pattern, const STRUTIL_CHARTYPE* string)
+/*
+ * Searches for the given pattern in the string and returns the index of the first matching character.
+ * If the pattern cannot be found, returns -1.
+ */
+ssize_t indexof(const STRUTIL_CHARTYPE *pattern, const STRUTIL_CHARTYPE *string)
 {
     return find(pattern, string);
 }
 
-// Perfected
-ssize_t rindexof (const STRUTIL_CHARTYPE* pattern, const STRUTIL_CHARTYPE* string)
+/*
+ * Searches for the given pattern in the string from the right and returns the index of the first matching
+ * character. If the pattern cannot be found, returns -1.
+ */
+ssize_t rindexof(const STRUTIL_CHARTYPE *pattern, const STRUTIL_CHARTYPE *string)
 {
+    ssize_t i;
+    size_t patternlen, stringlen;
+
     if (pattern == NULL || string == NULL)
         return -1;
-    if (*pattern==0)
+    if (*pattern == 0)
         return 0;
-    if (*string==0 && *pattern!=0)
+    if (*string == 0 && *pattern != 0)
         return -1;
-    size_t patternlen = STRLEN(pattern);
-    size_t stringlen = STRLEN(string);
+    patternlen = STRLEN(pattern);
+    stringlen = STRLEN(string);
     if (patternlen > stringlen)
         return -1;
-    
-    for (ssize_t i = stringlen - patternlen; i >= 0; --i)
-    {
-        if (STRNCMP(&string[i], pattern, patternlen) == 0)
+    for (i = stringlen - patternlen; i >= 0; --i) {
+        if (STRNCMP(string + i, pattern, patternlen) == 0)
             return i;
     }
-
     return -1;
-
-    /*
-    STRUTIL_CHARTYPE* revpattern = reversecopy(pattern);
-    STRUTIL_CHARTYPE* revstring = reversecopy(string);
-    ssize_t found_index = indexof(revpattern, revstring);
-    if (found_index != -1)
-        found_index = (STRLEN(revstring)-found_index)-STRLEN(revpattern);
-    free(revpattern);
-    free(revstring);
-    return found_index;
-    */
 }
 
-// Perfected
-STRUTIL_CHARTYPE* substring (size_t start, size_t end, const STRUTIL_CHARTYPE* string)
+/*
+ * Returns a pointer to a dynamically allocated character buffer containing a substring from the given string, 
+ * starting with index (start) and ending with index (end - 1).
+ */
+STRUTIL_CHARTYPE *substring(size_t start, size_t end, const STRUTIL_CHARTYPE *string)
 {
+    size_t maxindex, i, j;
+    STRUTIL_CHARTYPE *substr;
+
     if (string == NULL)
         return NULL;
-    if (equals(string, (STRUTIL_CHARTYPE*)""))
-    {
+    if (equals(string, (STRUTIL_CHARTYPE *)"")) {
         if (start == 0 && end == 0)
-            return (STRUTIL_CHARTYPE*)calloc(1, sizeof(STRUTIL_CHARTYPE));
+            return (STRUTIL_CHARTYPE *)calloc(1, sizeof(STRUTIL_CHARTYPE));
         else
             return NULL;
     }
-    size_t maxindex = STRLEN(string)-1;
+    maxindex = STRLEN(string) - 1;
     if (start > maxindex || end > maxindex || start > end)
         return NULL;
-    STRUTIL_CHARTYPE* substr = (STRUTIL_CHARTYPE*)malloc((end-start+1)*sizeof(STRUTIL_CHARTYPE));
+    substr = (STRUTIL_CHARTYPE *)calloc(nullterm(end - start + 1), sizeof(STRUTIL_CHARTYPE));
     if (substr == NULL)
         return NULL;
-    for (size_t i=start, j=0; i<end; ++i, ++j)
-    {
+    for (i = start, j = 0; i < end; ++i, ++j) {
         substr[j] = string[i];
     }
-    substr[end-start] = 0;
     return substr;
 }
--- strutil.h
+++ strutil.h
@@ -1,63 +1,118 @@
 #ifndef STRUTIL_H
-    #define STRUTIL_H
+#define STRUTIL_H
 
-    #ifdef __cplusplus
-        extern "C" {
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include <stdbool.h>
+
+#ifdef ENABLE_STRUTIL_WCHAR
+    #include <wchar.h>
+    #define STRLEN  wcslen
+    #define STRCMP  wcscmp
+    #define STRNCMP wcsncmp
+    #define STRCPY  wcscpy
+    #define STRNCPY wcsncpy
+    #define STRSTR  wcsstr
+    typedef wchar_t STRUTIL_CHARTYPE;
+#else
+    #include <string.h>
+    #define STRLEN  strlen
+    #define STRCMP  strcmp
+    #define STRNCMP strncmp
+    #define STRCPY  strcpy
+    #define STRNCPY strncpy
+    #define STRSTR  strstr
+    typedef char STRUTIL_CHARTYPE;
+#endif
+
+#ifdef _WIN32                          // Platform is Windows
+    #ifdef _MSC_VER                    // Compiler is MSVC
+        #include <BaseTsd.h>
+        typedef SSIZE_T ssize_t;
+    #elif (!defined(__MINGW32__))      // For non-MinGW compilers
+        typedef long int ssize_t;      // Fallback: alias long int to ssize_t
     #endif
-
-    #ifdef ENABLE_STRUTIL_WCHAR
-        #include <wchar.h>
-        #define STRLEN wcslen
-        #define STRCMP wcscmp
-        #define STRNCMP wcsncmp
-        #define STRCPY wcscpy
-        #define STRNCPY wcsncpy
-        #define STRSTR wcsstr
-        typedef wchar_t STRUTIL_CHARTYPE;
-    #else
-        #include <string.h>
-        #define STRLEN strlen
-        #define STRCMP strcmp
-        #define STRNCMP strncmp
-        #define STRCPY strcpy
-        #define STRNCPY strncpy
-        #define STRSTR strstr
-        typedef char STRUTIL_CHARTYPE;
-    #endif
-
-    #ifdef _WIN32                       // Platform is Windows
-        #if defined(_MSC_VER)           // Compiler is MSVC
-            #include <BaseTsd.h>
-            typedef SSIZE_T ssize_t;
-        #elif defined(__MINGW32__)      // MinGW compiler
-            #include <sys/types.h>
-        #else                           // For other less common/unknown compilers
-            typedef long int ssize_t;   // Fallback to type-aliasing with long int for ssize_t
+#else                                  // Non-Windows platforms
+    #if defined(__has_include)
+        #if (!__has_include(<unistd.h>))  // Implies a non-POSIX-compatible system
+            typedef long int ssize_t;     // Fallback: alias long int to ssize_t
         #endif
     #else
-        #ifdef __has_include
-            #if __has_include(<unistd.h>)   // Implies a POSIX-compatible system
-                #include <sys/types.h>
-            #else                           // Implies a non-POSIX system
-                typedef long int ssize_t;   // Fallback to type-aliasing with long int for ssize_t
-            #endif
-        #else                               // No __has_include (means no proper way to verify for POSIX)
-            typedef long int ssize_t;       // Fallback to type-aliasing with long int for ssize_t
-        #endif
+        #error "Please compile using a C/C++17 or newer compiler."
     #endif
+#endif
 
-    unsigned int equals (const STRUTIL_CHARTYPE* string1, const STRUTIL_CHARTYPE* string2);
-    STRUTIL_CHARTYPE* copy (const STRUTIL_CHARTYPE* string);
-    void reverse (STRUTIL_CHARTYPE* string);
-    STRUTIL_CHARTYPE* reversecopy (const STRUTIL_CHARTYPE* string);
-    int startswith (const STRUTIL_CHARTYPE* pattern, const STRUTIL_CHARTYPE* string);
-    int endswith (const STRUTIL_CHARTYPE* pattern, const STRUTIL_CHARTYPE* string);
-    ssize_t find (const STRUTIL_CHARTYPE* pattern, const STRUTIL_CHARTYPE* string);
-    ssize_t indexof (const STRUTIL_CHARTYPE* pattern, const STRUTIL_CHARTYPE* string);
-    ssize_t rindexof (const STRUTIL_CHARTYPE* pattern, const STRUTIL_CHARTYPE* string);
-    STRUTIL_CHARTYPE* substring (size_t start, size_t end, const STRUTIL_CHARTYPE* string);
+#define EOB ((void*)-1)     // End of buffer character for pointer buffers
 
-    #ifdef __cplusplus
-        }
-    #endif
+#define strbuflen(string)   (STRLEN(string) + 1)        // Gives the length of the string's buffer.
+#define nullterm(_Strlen)   (_Strlen + 1)               // Accounts for the null byte in string length functions.
+
+/*
+ * Checks if two strings are equal and returns 1 if true, 0 otherwise.
+ */
+bool equals (const STRUTIL_CHARTYPE* string1, const STRUTIL_CHARTYPE* string2);
+
+/* 
+ * Returns a pointer to a shallow copy of the given string. This function dynamically allocates
+ * memory for the string. This memory may need to be freed by the caller to avoid memory
+ * leaks. If memory allocation fails or the function receives a NULL pointer, returns NULL.
+ */
+STRUTIL_CHARTYPE* copy (const STRUTIL_CHARTYPE* string);
+
+/*
+ * Reverses the given string. This function attempts to modify the given string. Make sure
+ * that the string being passed is a modifiable character array or a dynamically allocated
+ * character buffer. Use reversecopy() for read-only strings.
+ */
+void reverse (STRUTIL_CHARTYPE* string);
+
+/*
+ * Creates a new dynamically allocated copy of the given string, reverses it, and returns
+ * a pointer to the newly created buffer. Needs to be freed by the caller to prevent
+ * memory leaks. If memory allocation fails or the function receives a NULL pointer,
+ * returns NULL.
+ */
+STRUTIL_CHARTYPE* reversecopy (const STRUTIL_CHARTYPE* string);
+
+/*
+ * Check if the given string starts with the given pattern. Returns 1 if true, 0 otherwise.
+ */
+bool startswith (const STRUTIL_CHARTYPE* pattern, const STRUTIL_CHARTYPE* string);
+
+/*
+ * Check if the given string ends with the given pattern. Returns 1 if true, 0 otherwise.
+ */
+bool endswith (const STRUTIL_CHARTYPE* pattern, const STRUTIL_CHARTYPE* string);
+
+/*
+ * Searches for the given pattern in the string and returns the index of the first matching character.
+ * If the pattern cannot be found, returns -1.
+ */
+ssize_t find (const STRUTIL_CHARTYPE* pattern, const STRUTIL_CHARTYPE* string);
+
+/*
+ * Searches for the given pattern in the string and returns the index of the first matching character.
+ * If the pattern cannot be found, returns -1.
+ */
+ssize_t indexof (const STRUTIL_CHARTYPE* pattern, const STRUTIL_CHARTYPE* string);
+
+/*
+ * Searches for the given pattern in the string from the right and returns the index of the first matching
+ * character. If the pattern cannot be found, returns -1.
+ */
+ssize_t rindexof (const STRUTIL_CHARTYPE* pattern, const STRUTIL_CHARTYPE* string);
+
+/*
+ * Returns a pointer to a dynamically allocated character buffer containing a substring from the given string, 
+ * starting with index (start) and ending with index (end - 1).
+ */
+STRUTIL_CHARTYPE* substring (size_t start, size_t end, const STRUTIL_CHARTYPE* string);
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif
